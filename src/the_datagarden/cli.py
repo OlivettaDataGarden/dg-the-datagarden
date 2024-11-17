c9e679c80dd6d30648a90d01b93abe2b6ccb1574
Maarten de Ruyter <devops@geodatagarden.com>
2024-11-17 11:33:44 +0100

    added pre commit

--- src/the_datagarden/cli.py
+++ src/the_datagarden/cli.py
@@ -1,8 +1,10 @@
 import click
 
+
 @click.command()
 @click.argument("arg1", required=False)
-def main(arg1):
+def main(arg1) -> int:
     """Command-line tool for my_package_name."""
     print(f"Received arguments: arg1={arg1}")
     print("Running your command...")
+    return 0
